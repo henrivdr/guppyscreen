b2a3090025882d0705a5012db622972ae327f76d
Henri van der Riet <henrivdr@gmail.com>
2025-03-04 21:42:05 +0200

    remove unused variable

--- src/prompt_panel.cpp
+++ src/prompt_panel.cpp
@@ -152,7 +152,7 @@ void PromptPanel::background() {
 void PromptPanel::handle_callback(lv_event_t *event) {
     lv_obj_t *btn = lv_event_get_current_target(event);
 
-    PromptPanel *panel = (PromptPanel*)event->user_data;
+    //Unused PromptPanel *panel = (PromptPanel*)event->user_data;
 
     lv_obj_t *label = lv_obj_get_child(btn, 0);
     lv_obj_t *command = lv_obj_get_child(btn, 1);
